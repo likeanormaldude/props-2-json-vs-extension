823ac018126f0ad5c28a0a9f2c43fa1dc5584668
Deivid Rodrigues <deivid.likeanormaldude@hotmail.com>
2025-07-18 13:10:19 -0400

    Props2Json Extension + Options

---
+++ Props2Json/Props2JsonExtension/ExtensionOptions/Props2JsonOptions..cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Props2JsonExtension.ExtensionOptions;
+
+[ProvideOptionPage(
+    typeof(Props2JsonOptions),
+    "Props2Json Extension", // Category name in Tools > Options
+    "General", // Page name
+    0,
+    0,
+    true
+)]
+[ComVisible(true)]
+public class Props2JsonOptions : DialogPage
+{
+    private bool _showNotifications = true;
+
+    public bool ShowNotifications
+    {
+        get => _showNotifications;
+        set => _showNotifications = value;
+    }
+}
---
+++ Props2Json/Props2JsonExtension/Props2JsonExtensionPackage.cs
@@ -0,0 +1,25 @@
+﻿global using System;
+global using Community.VisualStudio.Toolkit;
+global using Microsoft.VisualStudio.Shell;
+global using Task = System.Threading.Tasks.Task;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Props2JsonExtension.ExtensionOptions;
+
+namespace Props2JsonExtension;
+
+[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+[InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+[ProvideMenuResource("Menus.ctmenu", 1)]
+[Guid(PackageGuids.Props2JsonExtensionString)]
+[ProvideOptionPage(typeof(Props2JsonOptions), "Props2Json Extension", "General", 0, 0, true)]
+public sealed class Props2JsonExtensionPackage : ToolkitPackage
+{
+    protected override async Task InitializeAsync(
+        CancellationToken cancellationToken,
+        IProgress<ServiceProgressData> progress
+    )
+    {
+        await this.RegisterCommandsAsync();
+    }
+}
